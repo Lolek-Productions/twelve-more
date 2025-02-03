d53e2deda2215bad0408c8dc3563fb3e73a503f7
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-02-03 08:30:57 -0600

    joe and maria

--- src/app/(others)/page.js
+++ src/app/(others)/page.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div className="text-xl bg-blue-500 text-red-900">
-      Hello
+    <div className="text-xl bg-blue-500 text-white p-10">
+      Hello Joe and Maria
     </div>
   )
 }
