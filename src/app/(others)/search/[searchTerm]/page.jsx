093dfb7ee5039d59a9244a5dd76e2ac22f34010b
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-02-04 12:57:23 -0600

    change home page links throughout

--- src/app/(others)/search/[searchTerm]/page.jsx
+++ src/app/(others)/search/[searchTerm]/page.jsx
@@ -21,7 +21,7 @@ export default async function SearchPage({ params }) {
   return (
     <div>
       <div className='flex items-center space-x-2 py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200'>
-        <Link href={'/'} className='hover:bg-gray-100 rounded-full p-2'>
+        <Link href='/home' className='hover:bg-gray-100 rounded-full p-2'>
           <HiArrowLeft className='h-5 w-5' />
         </Link>
         <h2 className='sm:text-lg'>Back</h2>
