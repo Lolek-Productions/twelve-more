8518583979f8863e05e0c07c8a309ab64cdc3678
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-02-05 13:51:26 -0600

    added sort state on email

---
+++ src/app/admin/communities/page.jsx
@@ -0,0 +1,33 @@
+import { Payment, columns } from "./columns"
+import { DataTable } from "./data-table"
+
+async function getData() {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ]
+}
+
+export default async function DemoPage() {
+  const data = await getData()
+
+  return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">Communities</h3>
+        <p className="text-sm text-muted-foreground">
+          Stuff about the Community
+        </p>
+      </div>
+      <div className="container mx-auto">
+        <DataTable columns={columns} data={data} />
+      </div>
+    </div>
+  )
+}
