244f8f4d1df1964d75dc0243830289b343902a9e
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-02-18 15:51:55 -0600

    update route matcher

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)']);
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/', 'terms', 'privacy']);
 const isOnboardingRoute = createRouteMatcher(['/onboarding']);
 
 export default clerkMiddleware(async (auth, request) => {
