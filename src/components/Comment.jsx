e6af5a9c452b263a87f975975544dfcf84191deb
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-03-04 17:39:01 -0600

    hide 3 dots on comments.  Add back in link to user.

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -6,13 +6,13 @@ import { HiDotsHorizontal } from 'react-icons/hi';
 export default function Comment({ comment, id }) {
   return (
     <div className='flex p-3 border-b border-gray-200 hover:bg-gray-50 pl-10'>
-      {/*<Link href={`/users/${comment?.user?._id?.toString() || ''}`}>*/}
+      <Link href={`/users/${comment?.user?.id || '#'}`}>
         <img
           src={comment?.profileImg}
           alt='user-img'
           className='h-9 w-9 rounded-full mr-4'
         />
-      {/*</Link>*/}
+      </Link>
       <div className='flex-1'>
         <div className='flex items-center justify-between'>
           <div className='flex items-center space-x-1 whitespace-nowrap'>
@@ -24,7 +24,7 @@ export default function Comment({ comment, id }) {
               {moment(comment?.createdAt).fromNow()}
             </span>
           </div>
-          <HiDotsHorizontal className='text-sm' />
+          {/*<HiDotsHorizontal className='text-sm' />*/}
         </div>
         <p className='text-gray-800 text-xs my-3'>{comment?.comment}</p>
       </div>
