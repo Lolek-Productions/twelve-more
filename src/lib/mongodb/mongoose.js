ea4e94b375e3f698ac158ca17a3fafeacc9c04b5
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-03-05 10:31:28 -0600

    removed url parser

--- src/lib/mongodb/mongoose.js
+++ src/lib/mongodb/mongoose.js
@@ -9,8 +9,6 @@ export const connect = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
       dbName: 'twelve-more-app',
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
     });
     console.log('Connected to MongoDB');
   } catch (error) {
