e909069f7d27bf31272b132d42e5ff8e7ac2a801
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-03-10 10:53:37 -0500

    added graphic on home page

--- src/app/(public)/page.jsx
+++ src/app/(public)/page.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { Button } from "@/components/ui/button";
 import Link from 'next/link';
+import Image from "next/image.js";
 
 
 export default function Landing() {
   return (
     <div className="max-w-4xl mx-auto px-12 py-10 text-center bg-white rounded-lg">
-      <h1 className="text-[50px] font-bold text-gray-800 leading-normal">
-        GET HUNGRY
+      <div className="text-center">
+        <Image
+          src="/logo.png"
+          alt="TwelveMore"
+          className={'mx-auto'}
+          width={80}
+          height={80}
+          priority
+        />
+      </div>
+      <h1 className="mt-3 text-[50px] font-bold text-gray-800 leading-normal">
+        TwelveMore
       </h1>
       <p className=" text-2xl text-gray-600">
         Sign in and find your community
@@ -17,5 +28,5 @@ export default function Landing() {
         <Link href="/sign-up">Join Us</Link>
       </Button>
     </div>
-  )
+)
 }
