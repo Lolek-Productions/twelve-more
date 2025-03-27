fc26b70bbba4a7d8b800edbba25d9d10b3756d1a
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-03-27 08:54:05 -0500

    remove tanstack query devtools

--- src/components/TanstackQueryProvider.jsx
+++ src/components/TanstackQueryProvider.jsx
@@ -4,7 +4,6 @@ import React from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 export default function TanstackQueryProvider({ children }) {
-  // Create a client
   const [queryClient] = React.useState(() => new QueryClient({
     defaultOptions: {
       queries: {
@@ -15,7 +14,6 @@ export default function TanstackQueryProvider({ children }) {
   }))
 
   return (
-    // Provide the client to your App
     <QueryClientProvider client={queryClient}>
       {children}
     </QueryClientProvider>
