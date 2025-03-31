b9f44899755e98ccd264bf3db1dd64d5f315ab75
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-03-31 18:53:43 -0500

    improve label

--- src/components/VisibilityLabel.jsx
+++ src/components/VisibilityLabel.jsx
@@ -5,12 +5,12 @@ import { Badge } from "@/components/ui/badge"
 
 const VisibilityLabel = ({visibility}) => {
   return (
-    <>
+    <div>
       {visibility === 'private' ?
         <Badge className={'bg-gray-500 hover:bg-gray-500'}>Private</Badge> :
         <Badge className={'bg-blue-500 hover:bg-blue-500'}>Public</Badge>
       }
-    </>
+    </div>
   );
 };
 
