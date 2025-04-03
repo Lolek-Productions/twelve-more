30ebafb7d8405408d488cb73e0f1b6d34175b944
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-04-03 11:43:33 -0500

    add links from invite page

--- src/app/(main)/communities/[communityId]/invite/page.jsx
+++ src/app/(main)/communities/[communityId]/invite/page.jsx
@@ -208,7 +208,9 @@ export default function Invite() {
   return (
     <>
       <div className="py-2 px-3 sticky top-0 z-40 bg-white border-b border-gray-200">
-        <h2 className="text-lg sm:text-xl font-bold">Invite to {community?.name}</h2>
+        <Link href={`/communities/${community?.id}/posts`}>
+          <h2 className="text-lg sm:text-xl font-bold">Invite to {community?.name}</h2>
+        </Link>
       </div>
 
       <div className="p-5">
