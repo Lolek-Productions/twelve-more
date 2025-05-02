6606cd82d58f9c30c64a7d8cffd72f8c4dfcb23f
Fr. Josh McCarty <fr.mccarty@gmail.com>
2025-05-01 21:00:44 -0500

    Add daily system post cron job and update org/community IDs for production - TEMP

--- src/app/api/system-post/route.js
+++ src/app/api/system-post/route.js
@@ -6,11 +6,14 @@ export async function GET() {
   try {
     await connect();
     
-    const organizationId = "67e84cef001522de336670e9";
-    const communityId = "67e84ce27b99696289b14059";
+    // const organizationId = "67e84cef001522de336670e9"; //Test Org
+    // const communityId = "67e84ce27b99696289b14059"; //Test Community
+
+    const organizationId = "67ec452ecbdc464abad2a5ee"; //St. Carlo
+    const communityId = "67f635bb7620b7d0f173ba15"; //Founders
 
     const newPost = await Post.create({
-      text: "TEST - This is the daily system post.",
+      text: "Good Morning! - This is a test post from the system.",
       community: communityId,
       organization: organizationId,
     });
